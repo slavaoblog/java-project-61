b9f40bc6e40567d9389079d8cdf6582d3c7cf709
Viacheslav Oblog <slavaoblog@ya.ru>
2023-02-07 22:13:30 +0200

    added Makefile, Cli.java; updated App.java, build.gradle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,8 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-    }
+
+        Cli.greeting();
+
+        }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
