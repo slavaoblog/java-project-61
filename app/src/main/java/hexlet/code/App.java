9ad57876e08c3992432b865b1edc51b2919be195
Viacheslav Oblog <slavaoblog@ya.ru>
2023-03-07 20:26:58 +0200

    fixed checkstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 public class App {
     public static int gameNumber;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -18,21 +19,16 @@ public class App {
 
         gameNumber = scanner.nextInt();
 
-        if (gameNumber == 0) {
-            return;
-        } else if (gameNumber == 1) {
-            Engine.greeting();
-        } else if (gameNumber == 2) {
-            Even.numberTwo();
-        } else if (gameNumber == 3) {
-            Calc.numberThree();
-        } else if (gameNumber == 4) {
-            GCD.numberFour();
-        } else if (gameNumber == 5) {
-            Progression.numberFive();
-        } else if (gameNumber == 6) {
-            Prime.numberSix();
+        switch (gameNumber) {
+            case 0 -> {
+            }
+            case 1 -> Cli.greeting();
+            case 2 -> Even.numberTwo();
+            case 3 -> Calc.numberThree();
+            case 4 -> GCD.numberFour();
+            case 5 -> Progression.numberFive();
+            case 6 -> Prime.numberSix();
+            default -> System.out.println("Введен некорректный номер игры, введите еще раз.");
         }
-        scanner.close();
     }
 }
