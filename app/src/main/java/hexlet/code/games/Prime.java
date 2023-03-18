12df2cf9656b390058d9b77f6b60c2433473492b
Viacheslav Oblog <slavaoblog@ya.ru>
2023-03-18 20:56:11 +0200

    updated range of random number

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,7 +5,7 @@ import hexlet.code.Engine;
 public class Prime {
     public static void startGame() {
         for (int i = 0; i < 3; i++) {
-            int a = (int) (Math.random() * 100) + 1;
+            int a = (int) (Math.random() * 99) + 2;
             Engine.setQuestionAnswer(i,0, "Question: " + a);
             Engine.setQuestionAnswer(i,1, "yes");
             for (var j = 2; j <= a / 2; j++) {
